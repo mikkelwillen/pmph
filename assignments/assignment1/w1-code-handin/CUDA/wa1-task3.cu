6d7f8bc704922a35d96104bf738a937aac7a4ef8
mikkelwillen <mikkel@willen.dk>
2022-09-08 16:40:12 +0200

    cuda

--- assignments/assignment1/w1-code-handin/CUDA/wa1-task3.cu
+++ assignments/assignment1/w1-code-handin/CUDA/wa1-task3.cu
@@ -4,11 +4,18 @@
 #include <math.h>
 #include <cuda_runtime.h>
 
-__global__ void squareKernel(float* d_in, float *gpu_out) {
+__global__ void squareKernel(float* d_in, float* gpu_out) {
     const unsigned int lid = threadIdx.x; // local id inside a block
     const unsigned int gid = blockIdx.x*blockDim.x + lid; // global id
-    gpu_out[gid] = pow((d_in[gid]/(d_in[gid] - 2.3)), 3.0); // do computation
+    gpu_out[gid] = pow(d_in[gid]/(d_in[gid] - 2.3), 3.0); // do computation
 }
+
+__global__ void
+    squareSeq (unsigned int n, float* cpu_out) {
+        for (unsigned int i = 0; i < n; i++) {
+            cpu_out[i] = pow((float)i/((float)i - 2.3), 3.0)
+        }
+    }
 
 int main(int argc, char** argv) {
     unsigned int N = atoi(argv[1]);
@@ -18,6 +25,7 @@ int main(int argc, char** argv) {
     // allocate host memory
     float* h_in = (float*) malloc(mem_size);
     float* h_out = (float*) malloc(mem_size);
+    float* cpu_out = (float*) malloc(mem_size);
 
     // initialize the memory
     for (unsigned int i = 0; i < N; ++i) {
@@ -27,6 +35,7 @@ int main(int argc, char** argv) {
     // allocate device memory
     float* d_in;
     float* gpu_out;
+    
     cudaMalloc((void**)&d_in, mem_size);
     cudaMalloc((void**)&gpu_out, mem_size);
 
@@ -35,6 +44,9 @@ int main(int argc, char** argv) {
 
     // execute the kernel
     squareKernel<<< (blocksize - 1 + N)/blocksize, blocksize >>>(d_in, gpu_out);
+
+    // execute the seq
+    squareSeq(N, cpu_out);
 
     // copy result from device to host
     cudaMemcpy(h_out, gpu_out, mem_size, cudaMemcpyDeviceToHost);
