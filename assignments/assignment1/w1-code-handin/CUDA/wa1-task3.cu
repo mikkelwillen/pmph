45cca94661c833beaf8d3eaf3ae5557217908407
mikkelwillen <mikkel@willen.dk>
2022-09-08 16:06:32 +0200

    cuda

--- assignments/assignment1/w1-code-handin/CUDA/wa1-task3.cu
+++ assignments/assignment1/w1-code-handin/CUDA/wa1-task3.cu
@@ -5,21 +5,21 @@
 #include <cuda_runtime.h>
 
 __global__ void squareKernel(float* d_in, float *d_out) {
-    const unsigned int lid = threadIdx.x; // local id inside a block
-    const unsigned int gid = blockIdx.x*blockDim.x + lid; // global id
+    const unsigned long lid = threadIdx.x; // local id inside a block
+    const unsigned long gid = blockIdx.x*blockDim.x + lid; // global id
     d_out[gid] = pow((d_in[gid]/(d_in[gid] - 2.3)), 3.0); // do computation
 }
 
 int main(int argc, char** argv) {
-    unsigned int N = 128;
-    unsigned int mem_size = N*sizeof(float);
+    unsigned long N = 753411;
+    unsigned long mem_size = N*sizeof(float);
 
     // allocate host memory
     float* h_in = (float*) malloc(mem_size);
     float* h_out = (float*) malloc(mem_size);
 
     // initialize the memory
-    for (unsigned int i = 0; i < N; ++i) {
+    for (unsigned long i = 0; i < N; ++i) {
         h_in[i] = (float)i;
     }
 
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
     cudaMemcpy(h_out, d_out, mem_size, cudaMemcpyDeviceToHost);
 
     // print result
-    for (unsigned int i = 0; i < N; ++i) {
+    for (unsigned long i = 0; i < N; ++i) {
         printf("%d - %.6f\n", i, h_out[i]);
     }
 
