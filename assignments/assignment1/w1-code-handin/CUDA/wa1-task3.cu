70054c2899f529c6dfa4b0ce45e78506df4178d7
mikkelwillen <mikkel@willen.dk>
2022-09-12 16:30:25 +0200

    cuda

--- assignments/assignment1/w1-code-handin/CUDA/wa1-task3.cu
+++ assignments/assignment1/w1-code-handin/CUDA/wa1-task3.cu
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <sys/time.h>
+#include <time.h>
 #include <cuda_runtime.h>
 
 __global__ void squareKernel(float* d_in, float* gpu_out) {
@@ -16,10 +18,34 @@ void squareSeq (unsigned int n, float* cpu_out) {
         }
     }
 
+int timevalSubstract (struct timeval* result, 
+                       struct timeval* t2, 
+                       struct timeval* t1) {
+    unsigned int resolution = 1000000;
+    long int diff = (t2 -> tv_usec + resolution * t2 -> tv_sec) - 
+                    (t1 -> tv_usec + resolution * t1 -> tv_sec);
+    result -> tv_sec = diff / resolution;
+    result -> tv_usec = diff % resolution;
+    return (diff<0);
+}
+
+#define GPU_RUNS 100
 int main(int argc, char** argv) {
+    // initial values
     unsigned int N = 753411;
     unsigned int mem_size = N*sizeof(float);
     unsigned int blocksize = 256;
+
+    // variables to calc runtimes
+    unsigned long int elapsedP;
+    struct timeval tStartP;
+    struct timeval tEndP;
+    struct timeval tDiffP;
+
+    unsigned long int elapsedS;
+    struct timeval tStartS;
+    struct timeval tEndS;
+    struct timeval tDiffS;
 
     // allocate host memory
     float* h_in = (float*) malloc(mem_size);
@@ -41,11 +67,29 @@ int main(int argc, char** argv) {
     // copy host memory to device
     cudaMemcpy(d_in, h_in, mem_size, cudaMemcpyHostToDevice);
 
-    // execute the kernel
-    squareKernel<<< (blocksize - 1 + N) / blocksize, blocksize >>>(d_in, gpu_out);
+    // running the kernel
+    gettimeofday(&tStartP, NULL);
+
+    // execute kernel
+    for(int i = 0; i < GPU_RUNS, i++) {
+        squareKernel<<< (blocksize - 1 + N) / blocksize, blocksize >>>(d_in, gpu_out);
+    }
+    
+    gettimeofday(&tEndP, NULL);
+    timevalSubstract(&tDiffP, &tEndP, &tStartP);
+    elapsedP = (tDiffP.tv_sec*1e6 + tDiffP.tv_usec) / GPU_RUNS;
+
+    // running the sequential function
+    gettimeofday(%tStartS, NULL);
 
     // execute the seq
-    squareSeq(N, cpu_out);
+    for(int i = 0; i < GPU_RUNS, i++) {
+        squareSeq(N, cpu_out);
+    }
+
+    gettimeofday(&tEndS, NULL);
+    timevalSubstract(&tDiffS, &tEndS, &tStartS);
+    elapsedS = (tDiffP.tv_sec*1e6 + tDiffP.tv_usec) / GPU_RUNS;
 
     // copy result from device to host
     cudaMemcpy(h_out, gpu_out, mem_size, cudaMemcpyDeviceToHost);
@@ -54,7 +98,7 @@ int main(int argc, char** argv) {
     // print result
     for (unsigned int i = 0; i < N; ++i) {
         if (cpu_out[i] - h_out[i] > 0.0000001) {
-            printf("surt kurt\n");
+            printf("surt kurt, der er sgu for stor forskel\n");
             printf("%-6f - %.6f\n", cpu_out[i], h_out[i]);
             test = 0;
         }
@@ -62,6 +106,8 @@ int main(int argc, char** argv) {
 
     if(test) {
         printf("det virker sgu\n");
+        printf("Parallel took:   %.2fms\n", elapsedP.elapsed / 1000.0);
+        printf("Sequentiel took: %.2fms\n", elapsedP.elapsed / 1000.0);
     }
 
     // clean-up memory
