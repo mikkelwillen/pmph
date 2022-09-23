d565dd75006eff39a4aaeb49760dbe14232ea4ab
mikkelwillen <mikkel@willen.dk>
2022-09-23 15:48:35 +0200

    cuda a1

--- assignments/assignment2/w2-code-handin/cuda/pbbKernels.cu.h
+++ assignments/assignment2/w2-code-handin/cuda/pbbKernels.cu.h
@@ -434,7 +434,7 @@ copyFromGlb2ShrMem( const uint32_t glb_offs
                   , volatile T* shmem_inp ) {
     #pragma unroll
     for(uint32_t i = 0; i < CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x * CHUNK + i;
+        uint32_t loc_ind = i * blockDim.x + threadIdx.x;
         uint32_t glb_ind = glb_offs + loc_ind;
         T elm = ne;
         if(glb_ind < N) { elm = d_inp[glb_ind]; }
