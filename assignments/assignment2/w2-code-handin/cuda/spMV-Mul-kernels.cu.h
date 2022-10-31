88d67d112cfca775feda77c69d0f98b5c7adb059
mikkelwillen <mikkel@willen.dk>
2022-10-31 21:23:54 +0100

    assignment2

--- assignments/assignment2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
+++ assignments/assignment2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
@@ -14,8 +14,11 @@ __global__ void
 mkFlags(int mat_rows, int* mat_shp_sc_d, char* flags_d) {
     const unsigned int lid = threadIdx.x; // local id inside a block
     const unsigned int gid = blockIdx.x * blockDim.x + lid; // global id
+    if (gid == 0) {
+        flags_d[0] = 1;
+    }
     if (gid < mat_rows) {
-        flags_d[mat_shp_sc_d[gid]] = 1;
+        flags_d[mat_shp_sc_d[gid - 1]] = 1;
     }
 }
 
